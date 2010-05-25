78422aff67b588f1aba68c828dfb3308d6f3e790
jbaragry <jason.baragry@gmail.com>
2010-05-25 21:08:50 +0200

    oops

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe PagesController do
 
-  #Delete these examples and add some real ones
-  it "should use PagesController" do
-    controller.should be_an_instance_of(PagesController)
-  end
-
 
   describe "GET 'home'" do
     it "should be successful" do
