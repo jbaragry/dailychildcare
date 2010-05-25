9158dec65c01014916e40492c4d4887c08fa2f5c
jbaragry <jason.baragry@gmail.com>
2010-05-25 21:53:42 +0200

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  integrate_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+                               "Daily Childcare | Home")
     end
   end
 
@@ -15,5 +21,25 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+                               "Daily Childcare | Contact")
+    end
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title",
+                               "Daily Childcare | About")
+    end
+  end
+
 end
