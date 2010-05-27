b44556930f6681a00c97560ab7aa9db523e5ccd8
jbaragry <jason.baragry@gmail.com>
2010-05-27 11:20:16 +0200

    bad passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,20 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
-EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_presence_of :name, :email
   validates_format_of   :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates_confirmation_of :password
+
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,9 @@ describe User do
   before(:each) do
     @attr = {
       :name => "Parent",
-      :email => "parent@home.no"
+      :email => "parent@home.no",
+      :password => "pword",
+      :password_confirmation => "pword"
     }
   end
 
@@ -63,6 +65,33 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+
 
 
 
