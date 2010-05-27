1f548ea0be9a0b6607dd0d04aee38c2d42c58cf1
jbaragry <jason.baragry@gmail.com>
2010-05-27 11:22:04 +0200

    pword prob reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,12 +19,7 @@ class User < ActiveRecord::Base
   validates_format_of   :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
 
-  # Automatically create the virtual attribute 'password_confirmation'.
-  validates_confirmation_of :password
-
-  # Password validations.
-  validates_presence_of :password
-  validates_length_of   :password, :within => 6..40
+ 
 
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,7 @@ describe User do
     @attr = {
       :name => "Parent",
       :email => "parent@home.no",
-      :password => "pword",
-      :password_confirmation => "pword"
+      
     }
   end
 
@@ -65,34 +64,6 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-
-  describe "password validations" do
-
-    it "should require a password" do
-      User.new(@attr.merge(:password => "", :password_confirmation => "")).
-        should_not be_valid
-    end
-
-    it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).
-        should_not be_valid
-    end
-
-    it "should reject short passwords" do
-      short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short)
-      User.new(hash).should_not be_valid
-    end
-
-    it "should reject long passwords" do
-      long = "a" * 41
-      hash = @attr.merge(:password => long, :password_confirmation => long)
-      User.new(hash).should_not be_valid
-    end
-  end
-
-
-
 
 
 end
